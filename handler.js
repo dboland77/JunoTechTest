329532c74014b73e1081b802c0899ba0b4eefa33
Dave Boland <dboland77@gmail.com>
2022-01-20 09:59:43 +0000

    Updated handler (and test) to filter for matching O_ID

--- handler.js
+++ handler.js
@@ -1,6 +1,12 @@
 const handler = (event, context) => {
-  return JSON.parse(event.body);
-  
+  let shipments = JSON.parse(event.body);
+
+  // Need to filter the results for non-Juno orders
+  shipments.ORDERS = shipments.ORDERS.filter(
+    (order) => order.O_ID === order.OMS_ORDER_ID
+  );
+
+  return shipments;
 };
 
 module.exports = handler;
--- handler.test.js
+++ handler.test.js
@@ -1,22 +1,27 @@
-const fs = require('fs')
-const handler = require('./handler')
+const fs = require("fs");
+const handler = require("./handler");
 
 const event = {
-  body: fs.readFileSync('./fixtures/shipments.json', 'utf8')
-}
+  body: fs.readFileSync("./fixtures/shipments.json", "utf8"),
+};
 
 const context = {
-  accountReference: 'acme'
-}
+  accountReference: "acme",
+};
 
-describe('Handler', () => {
-  it('Parses the event data into JSON', () => {
-    const data = handler(event, context)
+describe("Handler", () => {
+  it("Parses the event data into JSON", () => {
+    const data = handler(event, context);
 
-    const { ORDERS: orders } = data
-    expect(orders.length).toEqual(3)
+    // Update the test because 2 results rather than 3 are now returned
+    const { ORDERS: orders } = data;
+    expect(orders.length).toEqual(2);
 
-    const [order] = orders
-    expect(order.O_ID).toEqual('12345')
-  })
-})
+    // Update the test to check for the matching order ID's
+    // O_ID 50022251 is excluded as the OMS_ORDERID = 135103229
+
+    const [order1, order2] = orders;
+    expect(order1.O_ID).toEqual("12345");
+    expect(order2.O_ID).toEqual("500324412");
+  });
+});
