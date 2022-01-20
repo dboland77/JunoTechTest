1e4085f349440954b3e3649feeab353911451fa1
Dave Boland <dboland77@gmail.com>
2022-01-20 12:10:28 +0000

    Removed console logs, checked results

--- handler.test.js
+++ handler.test.js
@@ -15,7 +15,7 @@ describe("Handler", () => {
 
     // Update the test because 2 results rather than 3 are now returned
     // Update to reflect the new return from handler
-    const allOrders = Object.keys(data).flatMap(key => data[key])
+    const allOrders = Object.keys(data).flatMap((key) => data[key]);
     expect(allOrders.length).toEqual(2);
 
     // Update the test to check for the matching order ID's
@@ -25,24 +25,27 @@ describe("Handler", () => {
     expect(order1.O_ID).toEqual("12345");
     expect(order2.O_ID).toEqual("500324412");
   });
-    it('Check cancelled orders have quantity = 0', () => {
-      const { _, cancelled_orders } = handler(event, context);
-      console.log(cancelled_orders)
-  
-      const [cancelledOrder] = cancelled_orders;
-  
-      const orderQuantities = cancelledOrder.ORDER_LINES.map(orderLine => parseInt(orderLine.QUANTITY));
-  
-      expect(Math.max.apply(this, orderQuantities)).toBe(0);
-    })
-  
-    it('Check remaining orders are fulfillments', () => {
-      const { fulfilled_orders, _ } = handler(event, context);
-  
-      const [fulfilledOrder] = fulfilled_orders;
-  
-      const orderQuantities = fulfilledOrder.ORDER_LINES.map(orderLine => parseInt(orderLine.QUANTITY));
-  
-      expect(Math.max.apply(this, orderQuantities)).toBeGreaterThanOrEqual(1);
-    })
+  it("Check cancelled orders have quantity = 0", () => {
+    const { _, cancelled_orders } = handler(event, context);
+
+    const [cancelledOrder] = cancelled_orders;
+
+    const orderQuantities = cancelledOrder.ORDER_LINES.map((orderLine) =>
+      parseInt(orderLine.QUANTITY)
+    );
+
+    expect(Math.max.apply(this, orderQuantities)).toBe(0);
   });
+
+  it("Check remaining orders are fulfillments", () => {
+    const { fulfilled_orders, _ } = handler(event, context);
+
+    const [fulfilledOrder] = fulfilled_orders;
+
+    const orderQuantities = fulfilledOrder.ORDER_LINES.map((orderLine) =>
+      parseInt(orderLine.QUANTITY)
+    );
+
+    expect(Math.max.apply(this, orderQuantities)).toBeGreaterThanOrEqual(1);
+  });
+});
