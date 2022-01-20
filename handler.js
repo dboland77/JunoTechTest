326cdb28f582058efd04b077db9545b3b6dc1561
Dave Boland <dboland77@gmail.com>
2022-01-20 11:55:44 +0000

    Logic created and tests passed for order cancel/fulfil, needs checking

--- handler.js
+++ handler.js
@@ -1,12 +1,33 @@
 const handler = (event, context) => {
   let shipments = JSON.parse(event.body);
 
-  // Need to filter the results for non-Juno orders
-  shipments.ORDERS = shipments.ORDERS.filter(
-    (order) => order.O_ID === order.OMS_ORDER_ID
-  );
+  const JunoOrders = filterNonJuno(shipments.ORDERS);
 
-  return shipments;
+  const fulfilled_orders = fulfilled(JunoOrders);
+  const cancelled_orders = cancelled(JunoOrders);
+
+  return { fulfilled_orders, cancelled_orders };
+};
+
+// Need to filter the results for non-Juno orders
+// Need to create separate filtered results so will change this to a function
+// and create fulfiled and cancelled ones
+
+const filterNonJuno = (orders) => {
+  return orders.filter((order) => order.O_ID === order.OMS_ORDER_ID);
+};
+
+const fulfilled = (orders) => {
+  return orders.filter((order) =>
+    order.ORDER_LINES.some((orderLine) => parseInt(orderLine.QUANTITY) > 0)
+  );
+};
+
+const cancelled = (orders) => {
+  return orders.filter(
+    (order) =>
+      !order.ORDER_LINES.some((orderLine) => parseInt(orderLine.QUANTITY) > 0)
+  );
 };
 
 module.exports = handler;
--- handler.test.js
+++ handler.test.js
@@ -14,14 +14,35 @@ describe("Handler", () => {
     const data = handler(event, context);
 
     // Update the test because 2 results rather than 3 are now returned
-    const { ORDERS: orders } = data;
-    expect(orders.length).toEqual(2);
+    // Update to reflect the new return from handler
+    const allOrders = Object.keys(data).flatMap(key => data[key])
+    expect(allOrders.length).toEqual(2);
 
     // Update the test to check for the matching order ID's
     // O_ID 50022251 is excluded as the OMS_ORDERID = 135103229
 
-    const [order1, order2] = orders;
+    const [order1, order2] = allOrders;
     expect(order1.O_ID).toEqual("12345");
     expect(order2.O_ID).toEqual("500324412");
   });
-});
+    it('Check cancelled orders have quantity = 0', () => {
+      const { _, cancelled_orders } = handler(event, context);
+      console.log(cancelled_orders)
+  
+      const [cancelledOrder] = cancelled_orders;
+  
+      const orderQuantities = cancelledOrder.ORDER_LINES.map(orderLine => parseInt(orderLine.QUANTITY));
+  
+      expect(Math.max.apply(this, orderQuantities)).toBe(0);
+    })
+  
+    it('Check remaining orders are fulfillments', () => {
+      const { fulfilled_orders, _ } = handler(event, context);
+  
+      const [fulfilledOrder] = fulfilled_orders;
+  
+      const orderQuantities = fulfilledOrder.ORDER_LINES.map(orderLine => parseInt(orderLine.QUANTITY));
+  
+      expect(Math.max.apply(this, orderQuantities)).toBeGreaterThanOrEqual(1);
+    })
+  });
